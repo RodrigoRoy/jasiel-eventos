60602d7757f7d43dae8852445211cf0cf95ed600
Rodrigo Colín <rodrigo.cln@ciencias.unam.mx>
2020-01-19 17:07:36 -0600

    nuevo modelo para evento que contempla listado de personas en relacion a sus asientos

--- dev-server/model/event-model.js
+++ dev-server/model/event-model.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 const eventSchema = new mongoose.Schema({
   name: String, // Nombre del evento
   layout: [{ // Lista de mesas
+    label: String,
     position: {x: Number, y: Number},
     size: {width: Number, height: Number},
     chairs: [{
+      label: String,
       x: Number,
       y: Number,
       size: Number,
@@ -16,12 +18,13 @@ const eventSchema = new mongoose.Schema({
   place: {type: String, enum: ['Salón La Olla', 'Salón La Joya']}, // Nombre del lugar
   amount: {type: Number, default: 150, enum: [150, 200, 250, 300, 350, 400, 450, 500]},
   date: {type: Date, default: Date.now}, // Fecha del evento
-  people: [{ // Lista de personas
+  people: [{ // Lista de personas invitadas
     name: String,
     limit: Number,
-    presence: Number
+    presence: Number,
+    chairs: [String] // Lista de sillas seleccionadas
   }],
-  guest: String, // Nombre de invitados
+  guest: String, // Nombre de anfitrión
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'user'}
 });
 eventSchema.set('timestamps', true);
